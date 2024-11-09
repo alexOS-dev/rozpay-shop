b634d8ec166d56e147c357a8ac544465536e8897
Alex Oliva <alexoliva87464@gmail.com>
2024-11-09 14:55:00 -0300

    feat: add brand to pagination

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -6,6 +6,7 @@ interface PaginationOptions {
   page?: number;
   take?: number;
   category?: string;
+  brand?: string;
   min_price?: number;
   max_price?: number;
   order?: 'asc' | 'desc';
@@ -15,6 +16,7 @@ export const getPaginatedProductsWithImages = async ({
   page = 1,
   take = 9,
   category,
+  brand,
   min_price,
   max_price,
   order = 'desc',
@@ -30,6 +32,18 @@ export const getPaginatedProductsWithImages = async ({
       where.category = {
         name: category,
       };
+    }
+
+    if (brand && brand !== 'all') {
+      // Primero obtenemos el ID de la marca usando el slug
+      const brandData = await prisma.brand.findFirst({
+        where: { slug: brand },
+        select: { id: true },
+      });
+
+      if (brandData) {
+        where.brandId = brandData.id;
+      }
     }
 
     if (min_price !== undefined || max_price !== undefined) {
@@ -56,6 +70,7 @@ export const getPaginatedProductsWithImages = async ({
         category: true, // Incluimos la categoría en la consulta
         condition: true,
         Color: true,
+        Brand: true,
       },
       where: where,
       orderBy: order ? { price: order } : undefined,
