bf6f9841fe843314a70b84435a7000ce697b473b
Alex Oliva <alexoliva87464@gmail.com>
2024-11-10 11:03:04 -0300

    feat: implement responsive filters & accordion filters for desktop & mobile displays

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -4,14 +4,9 @@ import { redirect } from 'next/navigation';
 import { getPaginatedProductsWithImages } from '@/actions';
 
 import {
-  BrandSelector,
-  CategorySelector,
-  ColorToggle,
-  FreeShippingSwitch,
-  OfferProductsSwitch,
-  PriceFilter,
+  AccordionFilter,
+  FilterContent,
   ProductGrid,
-  ProductOrderSelector,
   ProductsPagination,
   SearchBar,
   Title,
@@ -54,29 +49,28 @@ export default async function HomePage({ searchParams }: Props) {
 
   return (
     <>
-      <Title title='Tienda' subtitle='Todos los productos' className='mb-2' />
+      <Title
+        title='Tienda'
+        subtitle='Todos los productos'
+        className='mb-2 px-4'
+      />
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 px-4 md:px-0 lg:px-0'>
-        <div className='flex flex-col mt-5 col-span-1 gap-5'>
-          <ProductOrderSelector />
+      <div className='grid grid-cols-1 lg:grid-cols-4 gap-10 px-4 md:px-0 lg:px-0'>
+        {/* Mobile Filters */}
+        <div className='lg:hidden'>
+          <AccordionFilter>
+            <FilterContent />
+          </AccordionFilter>
+        </div>
 
-          <CategorySelector />
-
-          <PriceFilter />
-
-          <OfferProductsSwitch />
-
-          <FreeShippingSwitch />
-
-          <ColorToggle />
-
-          <BrandSelector />
+        {/* Desktop Filters */}
+        <div className='hidden lg:flex lg:flex-col mt-5 col-span-1 gap-5'>
+          <FilterContent />
         </div>
 
         {/* Search Input with Product Grid */}
         <div className='flex flex-col col-span-1 lg:col-span-3'>
           <SearchBar />
-
           <ProductGrid products={products} />
         </div>
       </div>
