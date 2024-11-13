cbb3ce4f5168e541ee54083629f94af53dbf12f9
Alex Oliva <alexoliva87464@gmail.com>
2024-11-13 12:59:47 -0300

    fix: add swap prop to fonts to correct font handling on deploy

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,8 +1,9 @@
 import { Inter, Allerta_Stencil } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
 export const titleFont = Allerta_Stencil({
   subsets: ['latin'],
   weight: ['400'],
+  display: 'swap',
 });
